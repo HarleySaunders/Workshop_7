e944c32431431518d665a7c865cdafc2bf1ba5f5
Harley_19957195 <19957195@students.latrobe.edu.au>
2021-09-08 19:14:15 +1000

    Testing successful!

--- src/test/java/au/harley/AppTest.java
+++ src/test/java/au/harley/AppTest.java
@@ -1,8 +1,6 @@
 package au.harley;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
@@ -10,32 +8,6 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
     public void testInputIsDivByFive()
     {
         assertTrue(App.isDivisableByFive(15)); //Assertion
